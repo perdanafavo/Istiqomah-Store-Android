cfc1d5ea70c85bf498141fc4050ab1a9f630401f
Favo Perdana H.S <perdanafavo97@gmail.com>
2020-04-11 02:37:07 +0700

    Splash, Login, Register

---
+++ app/src/main/java/com/example/istiqomahstore/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.istiqomahstore.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.istiqomahstore.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/istiqomahstore/activity/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.example.istiqomahstore.activity
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.istiqomahstore.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
