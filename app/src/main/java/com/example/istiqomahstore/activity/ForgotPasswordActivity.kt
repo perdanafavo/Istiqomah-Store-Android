d5bf327417320e571fb0c8e192b046d3f83def46
Favo Perdana H.S <perdanafavo97@gmail.com>
2020-04-11 20:00:52 +0700

    Update layout forgot dan respon

--- app/src/main/java/com/example/istiqomahstore/activity/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/istiqomahstore/activity/ForgotPasswordActivity.kt
@@ -2,6 +2,7 @@ package com.example.istiqomahstore.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.istiqomahstore.R
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
