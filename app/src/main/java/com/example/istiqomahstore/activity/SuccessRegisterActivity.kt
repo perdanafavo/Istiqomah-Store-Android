dd18e89b129b93d00e395c3c43919482aba676ae
Favo Perdana H.S <perdanafavo97@gmail.com>
2020-04-11 19:59:14 +0700

    Layout Forgot Password dan Layout Respon

---
+++ app/src/main/java/com/example/istiqomahstore/activity/SuccessRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.istiqomahstore.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuccessRegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_success_register)
+    }
+}
