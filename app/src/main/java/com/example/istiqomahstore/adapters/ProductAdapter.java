b344ceb3ed435e2a8e18cc3f54681a57b776896c
Favo Perdana H.S <perdanafavo97@gmail.com>
2020-04-20 18:01:52 +0700

    Fungsi add to cart main produk

--- app/src/main/java/com/example/istiqomahstore/adapters/ProductAdapter.java
+++ app/src/main/java/com/example/istiqomahstore/adapters/ProductAdapter.java
@@ -1,5 +1,8 @@
 package com.example.istiqomahstore.adapters;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,9 +24,17 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+
     private MainProductActivity mainProductActivity;
     private ArrayList<ProdukData> produkData;
     private String activityTitle;
+
+    private ImageView ivProduct;
+    private TextView tvProductName, tvQuantity, tvTotal;
+    private Button btnAddToCart, btnCancel, btnPlus, btnMinus;
+
+    private Dialog myDialog;
+
 
     public ProductAdapter(ArrayList<ProdukData> produkData){
         this.produkData =produkData;
@@ -55,10 +66,57 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
                     .into(holder.productImage);
             holder.productImage.setBackgroundResource(R.color.colorBlue);
         }
+
+        final int finalPosition = position;
         holder.addToCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Fungsi PopUp
+
+                myDialog = new Dialog(mainProductActivity);
+                myDialog.setContentView(R.layout.custom_popup_cart);
+
+                ivProduct = myDialog.findViewById(R.id.ivProduct);
+                tvProductName = myDialog.findViewById(R.id.tvProductName);
+                tvQuantity = myDialog.findViewById(R.id.tvQuantity);
+                tvTotal = myDialog.findViewById(R.id.tvTotal);
+                btnAddToCart = myDialog.findViewById(R.id.btnAddToCart);
+                btnCancel = myDialog.findViewById(R.id.btnCancel);
+                btnPlus = myDialog.findViewById(R.id.btnPlus);
+                btnMinus = myDialog.findViewById(R.id.btnMinus);
+
+                myDialog.setCanceledOnTouchOutside(false);
+
+                tvProductName.setText(produkData.get(finalPosition).getNama_produk());
+                tvTotal.setText(produkData.get(finalPosition).getHarga_satuan().toString());
+
+                if (produkData.get(finalPosition).getGambar() != null) {
+                    Picasso.get().load(ENVIRONMENT.FOTO_URL+produkData.get(finalPosition).getGambar())
+                            .error(R.drawable.ic_shopping_cart)
+                            .networkPolicy(NetworkPolicy.NO_CACHE, NetworkPolicy.NO_STORE)
+                            .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
+                            .into(ivProduct);
+                    ivProduct.setBackgroundResource(R.color.colorBlue);
+                }
+
+                btnCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //cancel
+                        myDialog.dismiss();
+                    }
+                });
+
+                btnAddToCart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //Add to cart
+                    }
+                });
+
+                if (myDialog.getWindow() != null) {
+                    myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                }
+                myDialog.show();
             }
         });
 
@@ -70,7 +128,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
         else return 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView productName, productPrice;
         ImageView productImage;
 
